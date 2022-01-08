0facd5db9245d94d0a626ab6f3449b4f7c4fb0ad
kentozaka46 <kentoball52@gmail.com>
2022-01-08 19:03:50 +0900

    コンポーネント分割

---
+++ src/atoms/Gallery.tsx
@@ -0,0 +1,13 @@
+import Image from './Image'
+
+function Gallery() {
+  return (
+    <div className="columns is-vcentered is-multiline">
+      <div className="column is-3">
+        <Image />
+      </div>
+    </div>
+  )
+}
+
+export default Gallery
---
+++ src/atoms/Main.tsx
@@ -0,0 +1,15 @@
+import Gallery from './Gallery'
+
+function Main() {
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <Gallery />
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Main
