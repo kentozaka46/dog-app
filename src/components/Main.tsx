0c962f2075be4651a2873149b93372e984838da0
kentozaka46 <kentoball52@gmail.com>
2022-01-09 10:35:04 +0900

    フォーム機能実装

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,16 @@
 import Gallery from './Gallery'
 import { useEffect, useState } from 'react'
 import { fetchImages } from '../usecase/api'
+import Form from '../atoms/Form'
 
 function Main() {
   const [urls, setUrls] = useState<string[] | null>(null)
+
+  function reloadImages(breed: string) {
+    fetchImages(breed).then((dogUrls) => {
+      setUrls(dogUrls)
+    })
+  }
 
   useEffect(() => {
     fetchImages('shiba').then((dogUrls: string[]) => {
@@ -13,6 +20,11 @@ function Main() {
 
   return (
     <main>
+      <section className="section">
+        <div className="container">
+          <Form onFormSubmit={reloadImages} />
+        </div>
+      </section>
       <section className="section">
         <div className="container">
           <Gallery urls={urls} />
